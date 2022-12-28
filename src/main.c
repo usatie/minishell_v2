a8dea40d31f0d9962fd9c199f53ffda326ac4e37
Shun Usami <usatie@gmail.com>
2022-12-28 17:10:04 +0900

    :recycle: Replace system with ft_system

--- src/main.c
+++ src/main.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <readline/readline.h>
+
+int	ft_system(char *command)
+{
+	pid_t	pid;
+	int		wstatus;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		// child
+		execlp("/bin/sh", "sh", "-c", command, NULL, NULL);
+		exit(1);
+	}
+	else
+	{
+		// parent
+		waitpid(pid, &wstatus, 0);
+	}
+	return (wstatus);
+}
 
 int	main(void)
 {
@@ -13,7 +34,7 @@ int	main(void)
 		line = readline("minishell$ ");
 		if (line == NULL)
 			break ;
-		status = system(line);
+		status = ft_system(line);
 		free(line);
 	}
 	exit(WEXITSTATUS(status));
