1f1528f5ea5f60a7bc8a13449d5e477eea81b509
Shun Usami <usatie@gmail.com>
2022-12-30 21:42:46 +0900

    Refactor expand.c
    - Refactor expand.c

--- src/expand.c
+++ src/expand.c
@@ -23,7 +23,47 @@ void	append_char(char **s, char c)
 	*s = new;
 }
 
-void	quote_removal(t_token *tok)
+void	remove_single_quote(char **dst, char **rest, char *p)
+{
+	if (*p == SINGLE_QUOTE_CHAR)
+	{
+		// skip quote
+		p++;
+		while (*p != SINGLE_QUOTE_CHAR)
+		{
+			if (*p == '\0')
+				assert_error("Unclosed single quote");
+			append_char(dst, *p++);
+		}
+		// skip quote
+		p++;
+		*rest = p;
+	}
+	else
+		assert_error("Expected single quote");
+}
+
+void	remove_double_quote(char **dst, char **rest, char *p)
+{
+	if (*p == DOUBLE_QUOTE_CHAR)
+	{
+		// skip quote
+		p++;
+		while (*p != DOUBLE_QUOTE_CHAR)
+		{
+			if (*p == '\0')
+				assert_error("Unclosed double quote");
+			append_char(dst, *p++);
+		}
+		// skip quote
+		p++;
+		*rest = p;
+	}
+	else
+		assert_error("Expected double quote");
+}
+
+void	remove_quote(t_token *tok)
 {
 	char	*new_word;
 	char	*p;
@@ -35,40 +75,18 @@ void	quote_removal(t_token *tok)
 	while (*p && !is_metacharacter(*p))
 	{
 		if (*p == SINGLE_QUOTE_CHAR)
-		{
-			// skip quote
-			p++;
-			while (*p != SINGLE_QUOTE_CHAR)
-			{
-				if (*p == '\0')
-					assert_error("Unclosed single quote");
-				append_char(&new_word, *p++);
-			}
-			// skip quote
-			p++;
-		}
+			remove_single_quote(&new_word, &p, p);
 		else if (*p == DOUBLE_QUOTE_CHAR)
-		{
-			// skip quote
-			p++;
-			while (*p != DOUBLE_QUOTE_CHAR)
-			{
-				if (*p == '\0')
-					assert_error("Unclosed single quote");
-				append_char(&new_word, *p++);
-			}
-			// skip quote
-			p++;
-		}
+			remove_double_quote(&new_word, &p, p);
 		else
 			append_char(&new_word, *p++);
 	}
 	free(tok->word);
 	tok->word = new_word;
-	quote_removal(tok->next);
+	remove_quote(tok->next);
 }
 
 void	expand(t_token *tok)
 {
-	quote_removal(tok);
+	remove_quote(tok);
 }
