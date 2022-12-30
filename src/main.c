7d5c6150531efd99efc4e503d1904a6c74b04843
Shun Usami <usatie@gmail.com>
2022-12-30 09:32:44 +0900

    Execute path
    - fork(), execve(), wait()
    - No tokenize, command without args

--- src/main.c
+++ src/main.c
@@ -5,11 +5,42 @@
 #include <readline/readline.h>
 
 void	todo(const char *msg) __attribute__((noreturn));
+void	fatal_error(const char *msg) __attribute__((noreturn));
+
+void	fatal_error(const char *msg)
+{
+	dprintf(STDERR_FILENO, "Fatal Error: %s\n", msg);
+	exit(1);
+}
 
 void	todo(const char *msg)
 {
 	dprintf(STDERR_FILENO, "TODO: %s\n", msg);
 	exit(255);
+}
+
+int	interpret(char *line)
+{
+	extern char	**environ;
+	char		*argv[] = {line, NULL};
+	pid_t		pid;
+	int			status;
+
+	pid = fork();
+	if (pid < 0)
+		fatal_error("fork");
+	else if (pid == 0)
+	{
+		// child process
+		execve(line, argv, environ);
+		fatal_error("execve");
+	}
+	else
+	{
+		// parent process
+		wait(&status);
+		return (status);
+	}
 }
 
 int	main(void)
@@ -23,7 +54,7 @@ int	main(void)
 			break ;
 		if (*line)
 			add_history(line);
-		todo("Interpret command");
+		interpret(line);
 		free(line);
 	}
 	exit(0);
