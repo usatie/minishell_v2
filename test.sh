c5728eb8ea10d30b291d618d73ef5e75acfaa674
Shun Usami <usatie@gmail.com>
2022-12-31 21:11:12 +0900

    '<<'
    - Here Document

--- test.sh
+++ test.sh
@@ -112,4 +112,10 @@ assert 'cat <hoge'
 assert 'pwd >>pwd.txt' 'pwd.txt'
 assert 'pwd >>pwd.txt \n pwd >>pwd.txt' 'pwd.txt'
 
+## Here Document
+assert 'cat <<EOF\nhello\nworld\nEOF\nNOPRINT'
+assert 'cat <<EOF<<eof\nhello\nworld\nEOF\neof\nNOPRINT'
+assert 'cat <<EOF\nhello\nworld'
+assert 'cat <<E"O"F\nhello\nworld\nEOF\nNOPRINT'
+
 cleanup
