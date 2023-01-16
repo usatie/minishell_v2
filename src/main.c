fb293aa9d6371c94509bb235f77ac727c53962e3
Shun Usami <usatie@gmail.com>
2023-01-17 08:09:23 +0900

    Execute filename
    - Search path for filename from $PATH

--- src/main.c
+++ src/main.c
@@ -1,11 +1,14 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <limits.h>
 
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 
 void	fatal_error(const char *msg) __attribute__((noreturn));
+void	err_exit(const char *location, const char *msg, int status) __attribute__((noreturn));
 
 void	fatal_error(const char *msg)
 {
@@ -13,10 +16,60 @@ void	fatal_error(const char *msg)
 	exit(1);
 }
 
-int	interpret(char *line)
+void	err_exit(const char *location, const char *msg, int status)
+{
+	dprintf(STDERR_FILENO, "minishell: %s: %s\n", location, msg);
+	exit(status);
+}
+
+char	*search_path(const char *filename)
+{
+	char	path[PATH_MAX];
+	char	*value;
+	char	*end;
+
+	value = getenv("PATH");
+	while (*value)
+	{
+		// /bin:/usr/bin
+		//     ^
+		//     end
+		bzero(path, PATH_MAX);
+		end = strchr(value, ':');
+		if (end)
+			strncpy(path, value, end - value);
+		else
+			strlcpy(path, value, PATH_MAX);
+		strlcat(path, "/", PATH_MAX);
+		strlcat(path, filename, PATH_MAX);
+		if (access(path, X_OK) == 0)
+		{
+			char	*dup;
+
+			dup = strdup(path);
+			if (dup == NULL)
+				fatal_error("strdup");
+			return (dup);
+		}
+		if (end == NULL)
+			return (NULL);
+		value = end + 1;
+	}
+	return (NULL);
+}
+
+void	validate_access(const char *path, const char *filename)
+{
+	if (path == NULL)
+		err_exit(filename, "command not found", 127);
+	if (access(path, F_OK) < 0)
+		err_exit(filename, "command not found", 127);
+}
+
+int	exec(char *argv[])
 {
 	extern char	**environ;
-	char		*argv[] = {line, NULL};
+	const char	*path = argv[0];
 	pid_t		pid;
 	int			wstatus;
 
@@ -26,7 +79,10 @@ int	interpret(char *line)
 	else if (pid == 0)
 	{
 		// child process
-		execve(line, argv, environ);
+		if (strchr(path, '/') == NULL)
+			path = search_path(path);
+		validate_access(path, argv[0]);
+		execve(path, argv, environ);
 		fatal_error("execve");
 	}
 	else
@@ -35,6 +91,15 @@ int	interpret(char *line)
 		wait(&wstatus);
 		return (WEXITSTATUS(wstatus));
 	}
+}
+
+int	interpret(char *const line)
+{
+	int		status;
+	char	*argv[] = {line, NULL};
+
+	status = exec(argv);
+	return (status);
 }
 
 int	main(void)
